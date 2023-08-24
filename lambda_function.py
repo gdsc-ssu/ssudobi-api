d5a557d5d91a445d6dc41ac479b80bb646b140c8
Jin <thinkjin99@gmail.com>
2023-08-24 15:20:29 +0900

    Add token checker

--- lambda_function.py
+++ lambda_function.py
@@ -39,13 +39,19 @@ def handler(event: dict, context: dict) -> dict | None:
     response = None
 
     try:
-        token = event["token"]  # 로그인 토큰 조회
+        token = event.get("token")  # 로그인 토큰 조회
+        if not token:
+            raise AssertionError("Token is not passed")  # 토큰이 없는 경우
+
         res = asyncio.run(main(token))  # 예약 현황 조회
         s3 = boto3.client("s3")
         s3.put_object(
             Bucket="ssudobi-cache", Key="cache", Body=json.dumps(res)
         )  # 캐시 업데이트
         response = {"StatusCode": 200, "data": res}
+
+    except AssertionError as e:
+        response = {"StatusCode": 422, "error": str(e)}
 
     except Exception as e:
         response = {"StatusCode": 500, "error": str(e)}
@@ -57,6 +63,6 @@ def handler(event: dict, context: dict) -> dict | None:
 if __name__ == "__main__":
     from login_session import login
 
-    token = asyncio.run(login("20180806", "kidok0714!"))
-    res = asyncio.run(main(token))
-    print(res)
+    # token = asyncio.run(login("20180806", "kidok0714!"))
+    res = asyncio.run(main("token"))
+    # print(token)
